8f9f79a95349bbbe20985b2e082043ef8af00290
Florent Langlois <langlois@synchrotron-soleil.fr>
2020-12-09 17:39:03 +0100

    added a cpp main with only LimaCore and plugin (ie w/o Tango)

---
+++ test/soleil/src/main.cpp
@@ -0,0 +1,127 @@
+//- C++
+#include <iostream>
+#include <string>
+#include <time.h> // clock, usleep
+
+//- LIMA
+#include <lima/HwInterface.h>
+#include <lima/CtControl.h>
+#include <lima/CtAcquisition.h>
+#include <lima/CtVideo.h>
+#include <lima/CtImage.h>
+#include <imXpadInterface.h>
+#include <imXpadCamera.h>
+
+
+//--------------------------------------------------------------------------------------
+// test main:
+
+//- 1st argument is the hostname 
+//- 2nd argument is the port 
+//- 3rd argument is the moduleMask
+//--------------------------------------------------------------------------------------
+int main(int argc, char *argv[])
+{
+	std::cout << "============================================" << std::endl;
+	std::cout << "Usage :./ds_TestLimaImXpad hostname port [moduleMask] " << std::endl << std::endl;
+
+	try
+	{
+        std::string hostname    = "cegitek";
+        int port                = 3456;
+        unsigned int module_mask = 0;
+
+		//read args of main 
+		switch (argc)
+		{   
+			case 3:
+			{
+				std::istringstream arg_hostname(argv[1]);
+				arg_hostname >> hostname;
+
+				std::istringstream arg_port(argv[2]);
+				arg_port >> port;
+			}
+			break;
+
+			case 4:
+			{
+				std::istringstream arg_hostname(argv[1]);
+				arg_hostname >> hostname;
+
+				std::istringstream arg_port(argv[2]);
+				arg_port >> port;
+
+				std::istringstream arg_module_mask(argv[3]);
+				arg_module_mask >> module_mask;
+			}
+			break;
+
+			default:
+			{
+                std::cout << "Error passing parameters: default values used" << std::endl;
+			}
+			break;
+		}
+
+		//        
+		std::cout << "============================================" << std::endl;
+		std::cout << "hostname      = " << hostname << std::endl;
+		std::cout << "port          = " << port << std::endl;
+		std::cout << "module_mask   = " << module_mask << std::endl;
+		std::cout << "============================================" << std::endl;
+
+		//initialize imXpad::Camera objects & Lima Objects
+		std::cout << "Creating Camera Object..." << std::endl;
+		lima::imXpad::Camera my_camera(hostname, port, module_mask);
+
+		std::cout << "Creating Interface Object..." << std::endl;
+		lima::imXpad::Interface my_interface(my_camera);
+
+		std::cout << "Creating CtControl Object..." << std::endl;
+		lima::CtControl my_control(&my_interface);
+
+		std::cout << "============================================" << std::endl;
+
+        lima::CtControl::Status ct_status;
+        lima::imXpad::Camera::XpadStatus xpad_status;
+        clock_t start,end;
+        std::string dummy;
+
+        while (1)
+        {
+            //GetDetectorStatus, GetDetectorType','GetDetectorModel', 'GetModuleMask', 'GetModuleNumber
+            std::cout << "############################################" << std::endl;
+            usleep(10000); //- sleep 10 ms
+
+            std::cout << "--------------------------------------------" << std::endl;
+            start = clock();
+            my_control.getStatus(ct_status);
+            end = clock();
+	        std::cout << "Ctcontrol.getStatus : Elapsed time  = " << (end - start) / 1000 << " (ms)" << std::endl;
+
+            std::cout << "--------------------------------------------" << std::endl;
+            start = clock();
+            my_camera.getStatus(xpad_status);
+            end = clock();
+	        std::cout << "imXpad::Camera.getStatus : Elapsed time  = " << (end - start) / 1000 << " (ms)" << std::endl;
+
+            std::cout << "--------------------------------------------" << std::endl;
+            start = clock();
+            my_camera.getDetectorType(dummy);
+            end = clock();
+	        std::cout << "imXpad::Camera.getDetectorType : Elapsed time  = " << (end - start) / 1000 << " (ms)" << std::endl;
+        }
+	}
+	catch (lima::Exception e)
+	{
+		std::cerr << "LIMA Error : " << e << std::endl;
+	}
+    catch (...)
+	{
+		std::cerr << "Unknown Error" << std::endl;
+	}
+
+	return 0;
+}
+//--------------------------------------------------------------------------------------
