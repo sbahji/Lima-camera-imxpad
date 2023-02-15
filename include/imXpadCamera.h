cf1767b9058f43cc44f807c65c4b5e8876b27d60
Florent Langlois <langlois@synchrotron-soleil.fr>
2023-02-15 19:00:28 +0100

    fix pb of image size & bump release number

--- include/imXpadCamera.h
+++ include/imXpadCamera.h
@@ -432,8 +432,6 @@ private:
     int                     m_chip_number;
     int                     m_burst_number;
     unsigned int            m_stack_images;
-    std::string				m_image_size_from_hardware;
-
 
     // Buffer control object
     SoftBufferCtrlObj       m_buffer_ctrl_obj;
